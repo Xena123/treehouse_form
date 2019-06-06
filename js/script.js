ac14234515dbcb85134178e40ffb917970661d91
Cheryl Casteling <cheryl.casteling@unlimtedgroup.com>
2019-06-06 15:44:30 +0100

    Got the color options working

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 
   // const otherTextInput = $("<input type=\"text\" id=\"other-title\" placeholder=\"Your Job Role\" />");;
   // $(firstfield).append(otherTextInput);
+
   const otherTextInput = $('#other-title');
 
   // Hide the other text input to begin with
@@ -14,9 +15,9 @@ $(document).ready(function() {
 
   // If the other option is selected then show the other text input
   // Else hide it
-  $('#title').change(function(){
-    $(this).find("option:selected").each(function(){
-      var optionValue = $(this).attr("value");
+  $('#title').change(function() {
+    $(this).find("option:selected").each(function() {
+      const optionValue = $(this).attr("value");
       if (optionValue === 'other') {
         $(otherTextInput).show();
       } else {
@@ -26,6 +27,32 @@ $(document).ready(function() {
   });
 
 
+  $('#design').change(function() {
+    $(this).find("option:selected").each(function() {
+      const designOptionValue = $(this).attr("value");
+      const colorOptions = $('#color option');
 
-
+      if (designOptionValue === 'js puns') {
+        $(colorOptions).each(function (index, value) {
+          let currentOptionText = ($(colorOptions[index]).text());
+          $(colorOptions[index]).hide();
+          if (currentOptionText.indexOf('Puns') >= 0) {
+            $(colorOptions[index]).show();
+          }
+        });
+      } else if (designOptionValue === 'heart js') {
+        $(colorOptions).each(function (index, value) {
+          let currentOptionText = ($(colorOptions[index]).text());
+          $(colorOptions[index]).hide();
+          if (currentOptionText.indexOf('I') >= 0) {
+            $(colorOptions[index]).show();
+          }
+        });
+      } else {
+        $(colorOptions).each(function (index, value) {
+          $(colorOptions[index]).show();
+        }); 
+      }
+    });
+  });
 });

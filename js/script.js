bb6251def15b4b4fd3805a2e96b65c1c54b54575
Cheryl Casteling <cheryl.casteling@unlimtedgroup.com>
2019-06-11 13:27:50 +0100

    Figured out the issue with the running total on the activites section

--- js/script.js
+++ js/script.js
@@ -7,10 +7,11 @@ $(document).ready(function() {
   // Hide the other text input to begin with
   otherTextInput.hide();
 
+  // *** OTHER JOB ROLE FUNCTIONALITY
   // If the "other" option is selected then show the "other" text input
   // Else hide it
-  $('#title').change(function() {
-    $(this).find("option:selected").each(function() {
+  $('#title').change(() => {
+    $(this).find("option:selected").each(() => {
       const optionValue = $(this).attr("value");
       if (optionValue === 'other') {
         otherTextInput.show();
@@ -20,6 +21,7 @@ $(document).ready(function() {
     });
   });
 
+  // *** T-SHIRT INFO FUNCTIONALITY
   // when the dropdown menu is changed
   $('#design').change(function() {
     // find the option that has been selected
@@ -31,7 +33,7 @@ $(document).ready(function() {
       // if the value of the option selected equals 'js puns'
       if (designOptionValue === 'js puns') {
         // then loop through all the option elements
-        $(colorOptions).each(function (index, value) {
+        $(colorOptions).each((index, value) => {
           let currentOptionText = ($(colorOptions[index]).text());
           // first hide all the option elements
           $(colorOptions[index]).hide();
@@ -42,7 +44,7 @@ $(document).ready(function() {
         });
         // the same for 'heart js'
       } else if (designOptionValue === 'heart js') {
-        $(colorOptions).each(function (index, value) {
+        $(colorOptions).each((index, value) => {
           let currentOptionText = ($(colorOptions[index]).text());
           $(colorOptions[index]).hide();
           if (currentOptionText.indexOf('I') >= 0) {
@@ -51,15 +53,18 @@ $(document).ready(function() {
         });
         // else reset and show all the options
       } else {
-        $(colorOptions).each(function (index, value) {
+        $(colorOptions).each((index, value) => {
           $(colorOptions[index]).show();
         }); 
       }
     });
   });
 
+
+  // *** ACTIVITIES SECTION FUNCTIONALITY
+  let runningTotal = 0;
   // when the fieldset with the class '.activites' is clicked or changed
-  $('.activities').change(function(event) {
+  $('.activities').change((event) => {
     let target = $(event.target);
     const activityLabels = document.querySelectorAll('.activities label');
     let clickedLabel = target.parent().text();
@@ -73,7 +78,7 @@ $(document).ready(function() {
     // and if the element clicked is an input
     if (target.is('input')) {
       // check to see if the text of the clicked input matches any of the other labels text
-      $(activityLabels).each(function (index, value) {
+      $(activityLabels).each((index, value) => {
         let currentLabelText = value.textContent;
         let currentValue = $(value).find('input')[0];
 
@@ -96,15 +101,51 @@ $(document).ready(function() {
       });
 
       if ((target).is(":checked")) {
-        let runningTotal = 0;
         runningTotal = runningTotal + amount;
-        const costElement = `<p>$${runningTotal}</p>`;
-        console.log(runningTotal);
+        const costElement = `<p class="total">$${runningTotal}</p>`;
+        if ($('.total')) {
+          $('.total').remove();
+        }
         $('.activities').append(costElement);
       }
     }
+  });
 
-    
+  // *** PAYMENT FUNCTIONALITY
+  const creditCardInfo = $('#credit-card');
+  const paypalInfo = $('#paypalInfo');
+  const bitcoinInfo = $('#bitcoinInfo');
+  // hide paypal and bitcoin info divs to begin with
+  paypalInfo.hide();
+  bitcoinInfo.hide();
+  // make sure you cannot select the first option of the payment method
+  $("#payment option[value=select_method]").attr('disabled', true);
+
+  // when an option from the payment dropdown menu is selected
+  $('#payment').change(() => {
+    // loop through the options
+    $(this).find("option:selected").each(function() {
+      const optionValue = $(this).attr("value");
+      // hide and show based on selection
+      if (optionValue === 'paypal') {
+        paypalInfo.show();
+        bitcoinInfo.hide();
+        creditCardInfo.hide();
+      } else if (optionValue === 'bitcoin') {
+        bitcoinInfo.show();
+        creditCardInfo.hide();
+        paypalInfo.hide();
+      } else if (optionValue === 'credit card') {
+        creditCardInfo.show();
+        bitcoinInfo.hide();
+        paypalInfo.hide();
+      } 
+    });
+  });
+
+  $('#submit').click((event) => {
+    event.preventDefault();
+    console.log('submit');
 
   });
 
